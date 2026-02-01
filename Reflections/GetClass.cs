12fd4275e7e507f3f232c57528be34b0595867a9
jaykant01 <jaykanty24092004@gmail.com>
2026-02-01 23:41:10 +0530

    [Jaykant] Add .Get Class Info using Reflections

---
+++ Reflections/GetClass.cs
@@ -0,0 +1,19 @@
+﻿namespace Reflections;
+
+public class GetClass
+{
+    public int Id;
+    public string Name;
+
+    public GetClass(int id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine("Student details");
+    }
+
+}
--- Reflections/Program.cs
+++ Reflections/Program.cs
@@ -1,9 +1,38 @@
-﻿namespace Reflections;
+﻿using System.Reflection;
+namespace Reflections;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // Problem 1
+        // Get class type
+        Type type = typeof(GetClass);
+        Console.WriteLine("Class Name: " + type.Name);
+
+        // Fileds
+        Console.WriteLine("\nFileds: ");
+        foreach (FieldInfo f in type.GetFields())
+        {
+            Console.WriteLine(f.Name);
+        }
+
+        // Constructors
+        Console.WriteLine("\nConstructors: ");
+        foreach (ConstructorInfo c in type.GetConstructors())
+        {
+            Console.WriteLine(c.Name);
+        }
+
+        // Methods
+        Console.WriteLine("\nMethods: ");
+        foreach (MethodInfo m in type.GetMethods())
+        {
+            Console.WriteLine(m.Name);
+        }
+
+
+
+        Console.ReadKey();
     }
 }
